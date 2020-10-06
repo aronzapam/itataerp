da181aab975e2699ca82fa5d603f9020815c3626
xciccx <72212701+xciccx@users.noreply.github.com>
2020-10-06 12:55:56 -0300

    adaptando adminLTE a patron mvc

---
+++ controladores/plantilla.controlador.php
@@ -0,0 +1,14 @@
+
+<!-- estamos creando una clase controlador  -->
+<?php
+
+class ControladorPlantilla{
+
+	public function ctrPlantilla(){
+
+		include "vistas/plantilla.php";
+
+	}	
+
+
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once "controladores/plantilla.controlador.php";
+
+$plantilla = new ControladorPlantilla();
+$plantilla -> ctrPlantilla();
+
+//creamos index para conectar todos los archivos de diferentes carpetas//
