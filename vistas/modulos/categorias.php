dc1b557a5d124c84d50c9a34e5b4bef1fa829305
xciccx <72212701+xciccx@users.noreply.github.com>
2020-10-14 13:41:10 -0300

    update

--- vistas/modulos/categorias.php
+++ vistas/modulos/categorias.php
@@ -4,13 +4,13 @@
     
     <h1>
       
-      administrar categorias
+      administrar categoria
     
     </h1>
 
     <ol class="breadcrumb">
       
-      <li><a href="#"><i class="fa fa-dashboard"></i> inicio</a></li>
+      <li><a href="inicio"><i class="fa fa-dashboard"></i> inicio</a></li>
       
       <li class="active">administrar categorias</li>
     
@@ -18,41 +18,23 @@
 
   </section>
 
-<<<<<<< HEAD
   <section class="content">
 
-=======
-  <!-- Main content -->
-  <section class="content">
-
-    <!-- Default box -->
->>>>>>> parent of 36ddda8... Merge branch 'main' of https://github.com/aronzapam/itataerp into main
     <div class="box">
+
       <div class="box-header with-border">
-<<<<<<< HEAD
   
         <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarUsuario">
           
-          agregar usuario
+          agregar categoria
 
         </button>
 
-=======
-        <h3 class="box-title">Title</h3>
-
-        <div class="box-tools pull-right">
-          <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                  title="Collapse">
-            <i class="fa fa-minus"></i></button>
-          <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-            <i class="fa fa-times"></i></button>
-        </div>
->>>>>>> parent of 36ddda8... Merge branch 'main' of https://github.com/aronzapam/itataerp into main
       </div>
+
       <div class="box-body">
-<<<<<<< HEAD
         
-       <table class="table table-bordered table-striped dt-responsive tablas" width="100%">
+       <table class="table table-bordered table-striped " >
          
         <thead>
          
@@ -139,9 +121,6 @@
 
         </table>
 
-=======
-        Start creating your amazing application!
->>>>>>> parent of 36ddda8... Merge branch 'main' of https://github.com/aronzapam/itataerp into main
       </div>
       <!-- /.box-body -->
       <div class="box-footer">
