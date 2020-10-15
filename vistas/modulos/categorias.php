3241629d51555da8ba7fc20b7a1f62fa79e4a713
xciccx <72212701+xciccx@users.noreply.github.com>
2020-10-15 11:50:26 -0300

    maquetacion categorias

--- vistas/modulos/categorias.php
+++ vistas/modulos/categorias.php
@@ -4,15 +4,15 @@
     
     <h1>
       
-      administrar categoria
+      Administrar categorias
     
     </h1>
 
     <ol class="breadcrumb">
       
-      <li><a href="inicio"><i class="fa fa-dashboard"></i> inicio</a></li>
+      <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
       
-      <li class="active">administrar categorias</li>
+      <li class="active">Administrar categorias</li>
     
     </ol>
 
@@ -24,9 +24,9 @@
 
       <div class="box-header with-border">
   
-        <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarUsuario">
+        <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarCategoria">
           
-          agregar categoria
+          Agregar categorias
 
         </button>
 
@@ -34,103 +34,151 @@
 
       <div class="box-body">
         
-       <table class="table table-bordered table-striped " >
+       <table class="table table-bordered table-striped dt-responsive tablas">
          
         <thead>
          
          <tr>
-              
-              <th style="width:10px">#</th>
-              <th>categoria</th>
-              <th>acciones</th>
+           
+           <th style="width:10px">#</th>
+           <th>categoria</th>
+           <th>Acciones</th>
 
-            </tr>
+         </tr> 
 
-          </thead>
+        </thead>
 
-          <tbody>
-            <tr>
-              
-                    <td>1</td>
-                    <td>EQUIPOS </td>
-                    <td>admin</td>
-                    <td><img src="vistas/img/usuarios/default/iconodefault.png" class="img-thumbnail" width="40px"></td>
-                    <td> Administrador</td>
-                    <td><button class="btn btn-success btn-xs"> activado</button></td>
-                    <td>2020-10-14 11:15</td>
-                    <td>
-
-                      <div class="btn-group">
-                        
-                        <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
-                        <button class="btn btn-danger"><i class="fa fa-times"></i></button>
-
-                      </div>
-
-                    </td>
-
-                  </tr>
-
+        <tbody>
+          
           <tr>
-              
-                    <td>1</td>
-                    <td>EQUIPOS ELECTROMECANICOS</td>
-                    <td>admin</td>
-                    <td><img src="vistas/img/usuarios/default/iconodefault.png" class="img-thumbnail" width="40px"></td>
-                    <td> Administrador</td>
-                    <td><button class="btn btn-success btn-xs"> activado</button></td>
-                    <td>2020-10-14 11:15</td>
-                    <td>
+            <td>1</td>
 
-                      <div class="btn-group">
-                        
-                        <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
-                        <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+            <td>equipos </td>
+            <td>
 
-                      </div>
-                      
-                    </td>
+              <div class="btn-group">
+                  
+                <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
 
-                  </tr>
+                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
 
+              </div>  
 
-        <tr>
-              
-                    <td>1</td>
-                    <td>EQUIPOS ELECTROMECANICOS</td>
-                    <td>admin</td>
-                    <td><img src="vistas/img/usuarios/default/iconodefault.png" class="img-thumbnail" width="40px"></td>
-                    <td> Administrador</td>
-                    <td><button class="btn btn-success btn-xs"> activado</button></td>
-                    <td>2020-10-14 11:15</td>
-                    <td>
+            </td>
 
-                      <div class="btn-group">
-                        
-                        <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
-                        <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+           <tr>
+            <td>1</td>
+            
+            <td>equipos </td>
+            <td>
 
-                      </div>
-                      
-             </td>
+              <div class="btn-group">
+                  
+                <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+
+                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+
+              </div>  
+
+            </td>
 
           </tr>
+
+            <tr>
+            <td>1</td>
             
+            <td>equipos </td>
+            <td>
 
-          </tbody>
+              <div class="btn-group">
+                  
+                <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
 
-        </table>
+                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+
+              </div>  
+
+            </td>
+
+          </tr>
+        </tbody>
+
+       </table>
 
       </div>
-      <!-- /.box-body -->
-      <div class="box-footer">
-        Footer
-      </div>
-      <!-- /.box-footer-->
+
     </div>
-    <!-- /.box -->
 
   </section>
-  <!-- /.content -->
+
 </div>
-<!-- /.content-wrapper -->
+
+<!--=====================================
+MODAL AGREGAR categoria
+======================================-->
+
+<div id="modalAgregarCategoria" class="modal fade" role="dialog">
+  
+  <div class="modal-dialog">
+
+    <div class="modal-content">
+
+      <form role="form" method="post" >
+
+        <!--=====================================
+        CABEZA DEL MODAL
+        ======================================-->
+
+        <div class="modal-header" style="background:#3c8dbc; color:white">
+
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+          <h4 class="modal-title">Agregar categoria</h4>
+
+        </div>
+
+        <!--=====================================
+        CUERPO DEL MODAL
+        ======================================-->
+
+        <div class="modal-body">
+
+          <div class="box-body">
+
+            <!-- ENTRADA PARA EL NOMBRE -->
+            
+            <div class="form-group">
+              
+              <div class="input-group">
+              
+                <span class="input-group-addon"><i class="fa fa-th"></i></span> 
+
+                <input type="text" class="form-control input-lg" name="nuevoCategoria" placeholder="Ingresar categoria" required>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+        <!--=====================================
+        PIE DEL MODAL
+        ======================================-->
+
+        <div class="modal-footer">
+
+          <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+
+          <button type="submit" class="btn btn-primary">Guardar categoria</button>
+
+        </div>
+
+      </form>
+
+    </div>
+
+  </div>
+
+</div>
